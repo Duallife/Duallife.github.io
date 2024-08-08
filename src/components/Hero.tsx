fe5f80be02f1594fe44878bd7e6eeca90e4012cf
Duallife <85776119+Duallife@users.noreply.github.com>
2024-08-08 23:26:35 +0800

    d1

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      <div className='text-center'>
+        <h1 className='text-6xl font-bold text-white'>Welcome to PROFO</h1>
+        <p className='text-xl text-white'>The best platform for your business</p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
